e2019b99f8a0adb257389dcf5dd5584f68ec7b4d
hbg1010 <harrisong26@parkschool.net>
2025-03-06 20:32:14 -0500

    oops :/

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -332,16 +332,16 @@ public class RobotContainer
         CoralDrive
       );
 
-      driverXbox2.b().whileTrue(
-        new InstantCommand(() -> {
-          m_AlgaeIntakeSubsystem.setSetpoint(0.2);
-        })
-      );
-      driverXbox2.b().onFalse(
-        new InstantCommand(() -> {
-          m_AlgaeIntakeSubsystem.setSetpoint(0.1);
-        })
-      );
+      // driverXbox2.b().whileTrue(
+      //   new InstantCommand(() -> {
+      //     m_AlgaeIntakeSubsystem.setSetpoint(0.2);
+      //   })
+      // );
+      // driverXbox2.b().onFalse(
+      //   new InstantCommand(() -> {
+      //     m_AlgaeIntakeSubsystem.setSetpoint(0.1);
+      //   })
+      // );
 
       driverXbox.leftBumper().onTrue(
         new InstantCommand(()->
