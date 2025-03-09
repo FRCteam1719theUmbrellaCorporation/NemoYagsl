93a3737bac3fe25b227254c769b9353ad1066c8b
matthewdgreen <matthewdgreen@users.noreply.github.com>
2025-03-09 14:51:31 -0400

    updated autos closer to human player

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -101,14 +101,13 @@ public final class Constants
 
     public static final double MIN_SPEED = -.5f;
     public static final double MAX_SPEED = .5f;
-
     public static final double coral_floorintake_pos = 0.36;
     public static final double coral_armdriving_pos = 0.15;
     public static final double coral_humanstatione_pos = 0.10;
     public static final double coral_reef_l1 = 0.2;
 
     public static final double coral_intake_floor_speed = 0.65f;
-    public static final double coral_intake_floor_speed_limited = 0.2f;
+    public static final double coral_intake_floor_speed_limited = 0.4f;
     public static final double coral_intake_humanStation_speed = 0.1f;
     public static final double coral_outtake_reef_speed = -0.35f;
 
